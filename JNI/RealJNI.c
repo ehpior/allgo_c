d1eae79ffc29f15026d48e1d291146985e10ad63
ehpior <gusrl0101@naver.com>
2021-03-01 22:54:47 +0900

    no message

--- JNI/RealJNI.c
+++ JNI/RealJNI.c
@@ -9,6 +9,19 @@
 #include <wchar.h>
 #include <sys/shm.h>
 
+//#include "real_cheg.h"
+//#include "real_program.h"
+
+typedef struct kospi{
+    int cnt;
+    char code[6];
+}kospi;
+
+typedef struct kosdaq{
+    int cnt;
+    char code[6];
+}kosdaq;
+
 /* 0 = read/write, 1 = read
 9 kospi_cnt, 99 kosdaq_cnt, 999 real_program, 9999 real_cheg */
 char *shm_get(key_t key, int size, int mode){
@@ -32,29 +45,45 @@ char *shm_get(key_t key, int size, int mode){
 
 JNIEXPORT void JNICALL Java_com_allgo_web_jni_RealJNI_updateCnt(JNIEnv *env, jobject obj)
 {
-    char *kospi_cnt_shm = shm_get(9, 10, 0);
+    /*char *kospi_cnt_shm = shm_get(9, 10, 0);
     char *kosdaq_cnt_shm = shm_get(99, 10, 0);
 
     sprintf(kospi_cnt_shm, "%d", 1500);
-    sprintf(kosdaq_cnt_shm, "%d", 2500);
+    sprintf(kosdaq_cnt_shm, "%d", 2500);*/
 
-    /*int kospi_cnt = atoi(shm_get(9, 4, 0));
-    int kosdaq_cnt = atoi(shm_get(99, 4, 0));*/
+
+    kospi *kospi_data = (kospi *)shm_get(9, sizeof(kospi), 0);
+    //kosdaq *kosdaq_data = (kosdaq *)shm_get(999, sizeof(kosdaq), 0);
+    if(kospi_data == NULL){
+        return ;
+    }
+
+    int kospi_cnt = kospi_data->cnt + 1;
+    //int kosdaq_cnt = kosdaq_data->cnt + 1;
+
+    kospi_data->cnt = kospi_cnt;
+    //kosdaq_data->cnt = kosdaq_cnt;
 
 	jclass cls = (*env)->GetObjectClass(env, obj);
-	jfieldID fid = (*env)->GetFieldID(env, cls, "kospi_cnt", "I");
-    jfieldID fid2 = (*env)->GetFieldID(env, cls, "kosdaq_cnt", "I");
 
-	(*env)->SetIntField(env, obj, fid, atoi(kospi_cnt_shm));
-    (*env)->SetIntField(env, obj, fid2, atoi(kosdaq_cnt_shm));
+	jfieldID fid = (*env)->GetFieldID(env, cls, "kospi_cnt", "I");
+	(*env)->SetIntField(env, obj, fid, kospi_cnt);
+
+    /*jfieldID fid2 = (*env)->GetFieldID(env, cls, "kosdaq_cnt", "I");
+    (*env)->SetIntField(env, obj, fid2, kosdaq_cnt);*/
 }
 
 JNIEXPORT void JNICALL Java_com_allgo_web_jni_RealJNI_updateRealCheg(JNIEnv *env, jobject obj, jobjectArray array)
 {
+    kospi *kospi_data = (kospi *)shm_get(9, sizeof(struct kospi), 0);
+
+    kospi_data->cnt = 1500;
 
 }
 
 JNIEXPORT void JNICALL Java_com_allgo_web_jni_RealJNI_updateRealProgram(JNIEnv *env, jobject obj, jobjectArray array)
 {
+    kosdaq *kosdaq_data = (kosdaq *)shm_get(999, sizeof(struct kosdaq), 0);
 
+    kosdaq_data->cnt = 2500;
 }
